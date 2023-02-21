ba9972dbeb8bda4d491ed5fc8d3a603996bd1fad
Guido Falsi <mad@madpilot.net>
2023-02-21 18:08:59 +0100

    Add a smaller font size.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['fe.xsl'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '2xs': ['0.625rem', '0.875rem'],
+      }
+    },
   },
   plugins: [],
 }
